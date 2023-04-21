6b2177d54cfe579b8520f3fb9621f7247f7ad46d
ocanath <ocanath@gmail.com>
2023-04-21 12:20:47 -0700

    labels

--- plot_ds1054z.py
+++ plot_ds1054z.py
@@ -7,5 +7,8 @@ t,data = load_ds1054z_csv('top_rings_voltage.csv')
 fig,ax = plt.subplots()
 for i in range(0,len(data)):
 	ax.plot(t,data[i])
+ax.set_ylabel('Volts')
+ax.set_xlabel('Seconds')
+ax.set_title('Oscilloscope Data')
 plt.show()
 
