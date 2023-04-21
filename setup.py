77135397e61f2c28b4bcee35f59cbd9487246fc6
ocanath <ocanath@gmail.com>
2023-04-21 12:14:31 -0700

    gitignore and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = [] # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+setup(name="mypackage", author='Jesse Cornman', version='1.0',install_requires=install_requires)
