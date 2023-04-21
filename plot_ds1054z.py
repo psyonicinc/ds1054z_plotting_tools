49c594bb81a930f7178080b449233c3a13c744a3
ocanath <ocanath@gmail.com>
2023-04-21 11:46:45 -0700

    plotting of top rings voltage

--- plot_ds1054z.py
+++ plot_ds1054z.py
@@ -1,2 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import sys 
+import fileinput
+import csv
+
+name = 'top_rings_voltage.csv'
+
+rd = []
+with open(name,newline='') as csvfile:
+	spamreader=csv.reader(csvfile,delimiter=',', quoting=csv.QUOTE_NONE)
+	for row in spamreader:
+		rd.append(row)
+
+datasize = np.size(rd,0)
+num_datas = np.size(rd,1)
+
+t = np.zeros(datasize-1)
+data = []
+for i in range(0,num_datas):
+	data.append(np.zeros(datasize-1))
+
+
+for row in range(1,datasize-1):
+	t[row] = float(rd[row][0])
+for row in range(1, datasize-1):
+	for col in range(0,num_datas-1):
+		data[col][row] = float(rd[row][col+1])
+
+fig,ax = plt.subplots()
+for i in range(0,num_datas):
+	ax.plot(t,data[i])
+plt.show()
+
